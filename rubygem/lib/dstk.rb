e961f8ad5e2cddd2e989a56427c572553fe46d2e
Cam Peterson <campeterson@gmail.com>
2013-11-21 16:55:07 -0700

    dstk ruby gem refactoring

--- rubygem/lib/dstk.rb
+++ rubygem/lib/dstk.rb
@@ -7,105 +7,74 @@ require 'rubygems' if RUBY_VERSION < '1.9'
 require 'json'
 require 'httparty'
 require 'httmultiparty'
+require 'debugger'
 
 module DSTK
   class DSTK
+
     def initialize(options = {})
       default_options = {
         :api_base => 'http://www.datasciencetoolkit.org',
         :check_version => true,
       }
 
-      if ENV['DSTK_API_BASE']
-        default_options[:api_base] = ENV['DSTK_API_BASE']
-      end
+      default_options[:api_base] = ENV['DSTK_API_BASE'] if ENV['DSTK_API_BASE']
 
       default_options.each do |key, value|
-        if !options.has_key?(key)
-          options[key] = value
-        end
+        options[key] = value if !options.has_key?(key)
       end
-          
+
       @dstk_api_base = options[:api_base]
 
-      if options[:check_version]
-        self.check_version()
-      end
+      self.check_version() if options[:check_version]
     end
 
-    # A short-hand method to URL encode a string. See http://web.elctech.com/?p=58
+    ############### UTILITIES ###################
+    # a short-hand method to URL encode a string. See http://web.elctech.com/?p=58
     def u(str)
       str.gsub(/[^a-zA-Z0-9_\.\-]/n) {|s| sprintf('%%%02x', s[0]) }
     end
 
-    def api_url(endpoint, arguments = {})
+    # build the api url
+    def api_url(endpoint)
       api_url = @dstk_api_base + endpoint
-      arguments_list = arguments.map do |name, value|
-        name + '=' + URI.encode(value)
-      end
-      if arguments_list.length > 0
-        arguments_string = '?' + arguments_list.join('&')
-        api_url += arguments_string
-      end
-
-      api_url
     end
 
-    def dstk_api_get(endpoint, arguments = {})
-      response = HTTParty.get(api_url(endpoint, arguments))
-
-      if !response.body or response.code != 200
-        raise "DSTK::dstk_api_get('#{endpoint}', #{arguments.to_json}) call to '#{api_url}' failed with code #{response.code} : '#{response.message}'"
-      end
-
-      result = nil
-      begin
-        result = JSON.parse(response.body)
-      rescue JSON::ParseError => e
-        raise "DSTK::dstk_api_get('#{endpoint}', #{arguments.to_json}, '#{data_payload}', '#{data_payload_type}') call to '#{api_url}' failed to parse response '#{response.body}' as JSON - #{e.message}"
-      end
-      if !result.is_a?(Array) and result['error']
-        raise result['error']
-      end
-      result
-    end
-
+    # convert payload to json if type json
     def prep_payload(data_payload, data_payload_type)
       data_payload_type == 'json' ? data_payload.to_json : data_payload
     end
 
-    def dstk_api_post(endpoint, arguments = {}, data_payload = nil, data_payload_type = 'json')
-      response = HTTParty.post(api_url(endpoint, arguments),
-                               { :body => prep_payload(data_payload, data_payload_type) })
+    # item should be an array. if it's not, make it one
+    def ensure_array(item)
+      item.is_a?(Array) ? item : [item]
+    end
+    ############### end UTILITIES ###############
 
-      if !response.body or response.code != 200
-        raise "DSTK::dstk_api_post('#{endpoint}', #{arguments.to_json}, '#{data_payload}', '#{data_payload_type}') call to '#{api_url}' failed with code #{response.code} : '#{response.message}'"
+    def dstk_api_call(endpoint, arguments = {}, data_payload = nil, data_payload_type = 'json')
+      response = if data_payload
+                   HTTParty.post(api_url(endpoint),
+                                 { :body => prep_payload(data_payload, data_payload_type),
+                                   :query => arguments })
+                 else
+                   HTTParty.get(api_url(endpoint), query: arguments)
+                 end
+
+      unless response.body && response.code == 200
+        raise "DSTK::dstk_api_call('#{endpoint}', #{arguments.to_json}, '#{data_payload}', '#{data_payload_type}') call to '#{response.request.uri.to_s}' failed with code #{response.code} : '#{response.message}'"
       end
 
-      result = nil
       begin
         result = JSON.parse(response.body)
+        !result.is_a?(Array) && result['error'] ? raise(result['error']) : result
       rescue JSON::ParseError => e
-        raise "DSTK::dstk_api_post('#{endpoint}', #{arguments.to_json}, '#{data_payload}', '#{data_payload_type}') call to '#{api_url}' failed to parse response '#{response.body}' as JSON - #{e.message}"
-      end
-      if !result.is_a?(Array) and result['error']
-        raise result['error']
-      end
-      result
-    end
-
-    def ensure_array(item)
-      if !item.is_a?(Array)
-        [item]
-      else
-        item
+        raise "DSTK::dstk_api_call('#{endpoint}', #{arguments.to_json}, '#{data_payload}', '#{data_payload_type}') call to '#{response.request.uri.to_s}' failed to parse response '#{response.body}' as JSON - #{e.message}"
       end
     end
 
-    ############### non data_payload requests ############
     def check_version
       required_version = 50
-      response = dstk_api_get('/info')
+      response = dstk_api_call('/info')
       actual_version = response['version']
       if actual_version < required_version
         raise "DSTK: Version #{actual_version.to_s} found but #{required_version.to_s} is required"
@@ -113,46 +82,53 @@ module DSTK
     end
 
     def geocode(address)
-      dstk_api_get('/maps/api/geocode/json', { 'address' => address })
+      dstk_api_call('/maps/api/geocode/json', { 'address' => address })
     end
 
-    ############### data_payload requests ################
     def ip2coordinates(ips)
-      dstk_api_post('/ip2coordinates', {}, ensure_array(ips), 'json')
+      dstk_api_call('/ip2coordinates', {}, ensure_array(ips), 'json')
     end
+
     def street2coordinates(addresses)
-      dstk_api_post('/street2coordinates', {}, ensure_array(addresses), 'json')
+      dstk_api_call('/street2coordinates', {}, ensure_array(addresses), 'json')
     end
+
     def coordinates2politics(coordinates)
-      dstk_api_post('/coordinates2politics', {}, coordinates, 'json')
+      dstk_api_call('/coordinates2politics', {}, coordinates, 'json')
     end
+
     def text2places(text)
-      dstk_api_post('/text2places', {}, text, 'string')
+      dstk_api_call('/text2places', {}, text, 'string')
     end
+
     def file2text(inputfile)
-      dstk_api_post('/text2places', {}, {:inputfile => inputfile}, 'file')
+      dstk_api_call('/text2places', {}, {:inputfile => inputfile}, 'file')
     end
+
     def text2sentences(text)
-      dstk_api_post('/text2sentences', {}, text, 'string')
+      dstk_api_call('/text2sentences', {}, text, 'string')
     end
+
     def html2text(html)
-      dstk_api_post('/html2text', {}, html, 'string')
+      dstk_api_call('/html2text', {}, html, 'string')
     end
-    # TODO: fix failing test
+
     def html2story(html)
-      dstk_api_post('/html2story', {}, html, 'string')
+      dstk_api_call('/html2story', {}, html, 'string')
     end
-    # TODO: fix failing test
+
     def text2people(text)
-      dstk_api_post('/text2people', {}, text, 'string')
+      dstk_api_call('/text2people', {}, text, 'string')
     end
+
     def text2times(text)
-      dstk_api_post('/text2times', {}, text, 'string')
+      dstk_api_call('/text2times', {}, text, 'string')
     end
+
     def text2sentiment(text)
-      dstk_api_post('/text2sentiment', {}, text, 'string')
+      dstk_api_call('/text2sentiment', {}, text, 'string')
     end
-    # TODO: fix failing test
+
     def coordinates2statistics(coordinates, statistics = nil)
       if statistics
         if !statistics.is_a?(Array) then statistics = [statistics] end
@@ -161,7 +137,7 @@ module DSTK
         arguments = {}
       end
 
-      dstk_api_post('/coordinates2statistics', arguments, coordinates, 'json')
+      dstk_api_call('/coordinates2statistics', arguments, coordinates, 'json')
     end
 
   end
